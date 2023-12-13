a00f3c114ce578922aec1743d8ceffca194e57f5
Elisa <elisosic@gmail.com>
2023-12-13 20:37:50 +0100

    updated axios functionality in js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
+function updateWeather (response) {
+    console.log(response.data);
+}
+
+function searchCity(city) {
+    let apiKey = "3c836392eab01t60930bboe42ecabfe4";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(updateWeather);
+}
 function handleSearchSubmit (event) {
     event.preventDefault();
     let searchInput = document.querySelector("#city-input");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit)
+searchFormElement.addEventListener("submit", handleSearchSubmit);
