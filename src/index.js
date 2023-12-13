2b03779ae5c69e9e93990bbc824d766d609d7fd8
Elisa <elisosic@gmail.com>
2023-12-13 19:43:53 +0100

    fixed submit form problem in html and fixed it in css, working on the js functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-alert (`hello`);
+function handleSearchSubmit (event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#city-input");
+    console.log(searchInput.value);
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit)
