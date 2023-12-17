03154af1f9efa6cc2116c3fa9a06e27d42316529
Elisa <elisosic@gmail.com>
2023-12-17 21:03:49 +0100

    Added forcast html throu js for all 5 dayas

--- src/index.js
+++ src/index.js
@@ -50,7 +50,36 @@ function handleSearchSubmit (event) {
     searchCity(searchInput.value);
 }
 
+function displayForecast () {
+    let days = ["Tue", "Wed", 
+    "Thu", "Fri", "Sat"];
+    let forecastHtml = "";
+
+    days.forEach(function(day) {
+        forecastHtml = forecastHtml + 
+        `
+        <div class="weather-forecast-day">
+        <div class="weather-forecast-date">
+        ${day} </div>
+        <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/few-clouds-night.png" width="50" /> 
+        <div class="weather-forecast-temperature"> 
+         <span class="weather-forecast-temperature-max">18° </span>  
+        <span class="weather-forecast-temperature-min"> 12°</span>
+      </div> 
+      </div>
+    `;
+    });
+
+    let forecastElement = document.querySelector ("#weather-forecast");
+
+    forecastElement.innerHTML = forecastHtml;
+    
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Trieste");
+
+displayForecast ();
+
